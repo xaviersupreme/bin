e9b7b0a2a8cd367beba67a8cba21fc5f99f1bd8a
Xavier Klavakian <162805460+xaviersupreme@users.noreply.github.com>
2026-05-31 01:01:19 -0700

    Update white_folk.cpp

--- C++/white_folk.cpp
+++ C++/white_folk.cpp
@@ -7,8 +7,21 @@
 #include <string>
 #include <fstream>
 #include <iomanip>
+#include <sstream>
 
 using namespace std;
+
+vector<string> split(const string& s, char delimiter) {
+    vector<string> parts;
+    stringstream ss(s);
+    string item;
+
+    while (getline(ss, item, delimiter)) {
+        parts.push_back(item);
+    }
+
+    return parts;
+}
 
 int main() {
     string path;
@@ -53,9 +66,8 @@ int main() {
             ascii += bytes[i];
         }
 
-        size_t used = ascii.size();
-        size_t missing = 16 - used;
-
+        size_t missing = 16 - ascii.size();
+ 
         if (ascii.size() % 16 == 0) {
             cout << "  " << ascii;
             ascii.clear();
@@ -64,7 +76,40 @@ int main() {
             cout << string(missing * 6 + 2, ' ');
             cout << ascii;
         }
-        
+
+    }
+
+    string bytes_input;
+    cin.clear();
+    cout << "\n" << "Search for hex (sepereate by ' '): ";
+    getline(cin, bytes_input);
+
+    vector<string> splitted = split(bytes_input, ' ');
+
+    for (size_t i{}; i < bytes.size(); ++i) {
+        int int_c = static_cast<int>(bytes[i]);
+        size_t rounded = (i + 15) & ~15; // round to the nearest 10th
+
+        stringstream stream;
+
+        if (int_c <= 15) {
+            stream << "0x0" << hex << uppercase << int_c;
+        } else if (int_c > 15) {
+            stream << "0x" << hex << uppercase << int_c;
+        }
+
+        string result( stream.str() ); // hexified int
+
+        int cur{};
+        for (const auto& split : splitted) {
+            if (result == static_cast<const string&>(split)) {
+                cout << "\n" << "FOUND @ OFFSET: " << setw(8) << setfill('0') << rounded;
+
+                /* TERRIBLE IMPLEMENTATION BUT I DONT KNOW HOW TO DO IT BETTER */
+                i += 16 - cur; // skip to the end of the line
+                cur = 0; // clear ascii for the next line
+            }
+        }
     }
 
     return 0;
