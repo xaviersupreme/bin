4aacf8a84087ebb0253648d99a098176e3dd5116
Xavier Klavakian <162805460+xaviersupreme@users.noreply.github.com>
2026-06-16 14:48:32 -0700

    Update arena_alloc.cpp

--- C++/arena_alloc.cpp
+++ C++/arena_alloc.cpp
@@ -1,172 +1,85 @@
+#include <cstddef>
+#include <cstdlib>
 #include <cstdint>
-#include <cstdio>
-#include <cstring>
+#include <iostream>
+#include <stdexcept>
 
-using namespace std;
-
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
 typedef uint64_t u64;
-typedef int8_t i8;
-typedef int16_t i16;
-typedef int32_t i32;
-typedef int64_t i64;
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t u8;
 
-typedef i8 b8;
-typedef i32 b32;
-typedef float f32;
-
-#define KiB(n) ((u64)(n) << 10)
-#define MiB(n) ((u64)(n) << 20)
-#define GiB(n) ((u64)(n) << 30)
-
-#define MIN(a,b) (((a) < (b)) ? (a) : (b))
-#define MAX(a,b) (((a) > (b)) ? (a) : (b))
-#define ALIGN_UP_POW(n,p) (((u64)(n) + ((u64)(p) - 1)) & (~((u64)(p) - 1)))
-
-#define ARENA_BASE_POS sizeof(mem_arena)
-#define ARENA_ALIGN (sizeof(void*))
-
-typedef struct {
-    u64 reserve_size;
-    u64 commit_size;
-    u64 pos;
-    u64 commit_pos;
+typedef struct mem_arena {
+    u8* memory_block;
+    
+    u64 capacity;
+    u64 offset;
 } mem_arena;
 
-mem_arena* arena_create(u64 reserve_size, u64 commit_size);
-void arena_destroy(mem_arena* arena);
+#define KiB(n) ((u64)n << (10))
+#define MiB(n) ((u64)n << (20))
+#define GiB(n) ((u64)n << (30))
 
-void* arena_push(mem_arena* arena, u64 size, b32 non_zero);
-void arena_pop(mem_arena* arena, u64 size);
-void arena_pop_to(mem_arena* arena, u64 pos);
-void arena_clear(mem_arena* arena);
-
-#define PUSH_STRUCT(arena, T) (T*)arena_push((arena), sizeof(T), false);
-#define PUSH_STRUCT_N2(arena, T) (T*)arena_push((arena), sizeof(T), true);
-#define PUSH_ARRAY(arena, T, n) (T*)arena_push((arena), sizeof(T) * (n), false);
-#define PUSH_ARRAY_N2(arena, T, n) (T*)arena_push((arena), sizeof(T) * (n), true);
-
-u32 plat_get_pagesize(void);
-void* plat_mem_reserve(u64 size);
-b32 plat_mem_commit(void* ptr, u64 size);
-b32 plat_mem_decommit(void* ptr, u64 size);
-b32 plat_mem_release(void* ptr, u64 size);
-
-
+mem_arena init_arena(u64 capacity);
+void* alloc_arena(mem_arena* arena, u64 size);
+void reset_arena(mem_arena* arena);
+void void_arena(mem_arena* arena);
 
 int main(void) {
-    mem_arena* perm_arena = arena_create(GiB(7), GiB(1));
+    mem_arena perm_arena = init_arena(KiB(1));
 
-    while (1) {
-        arena_push(perm_arena, MiB(16), false);
-        getc(stdin);
-    } 
+    u64* ptr = (u64*)alloc_arena(&perm_arena, sizeof(u64));
+    *ptr = 19;
 
-    arena_destroy(perm_arena);
-    return 0;
+    printf("*ptr: %llu, raw ptr = %p\n", *ptr, ptr);
 }
 
-
-
-mem_arena* arena_create(u64 reserve_size, u64 commit_size) {
-    u32 pagesize = plat_get_pagesize();
-
-    reserve_size = ALIGN_UP_POW(reserve_size, pagesize);
-    commit_size = ALIGN_UP_POW(commit_size, pagesize);
-
-    mem_arena* arena = (mem_arena*)plat_mem_reserve(reserve_size);
-    if (!arena) return NULL;
-
-    if (!plat_mem_commit(arena, commit_size)) {
-        plat_mem_release(arena, reserve_size);
-        return NULL;
+mem_arena init_arena(u64 capacity) {
+    if (capacity == 0) {
+        throw std::runtime_error("Attempt to set Arena capacity to or less than 0");
     }
 
-    arena->reserve_size = reserve_size;
-    arena->commit_size = commit_size;
-    arena->pos = ARENA_BASE_POS;
-    arena->commit_pos = commit_size;
+    mem_arena arena;
+    arena.memory_block = (u8*)malloc(capacity);
+    arena.capacity = capacity;
+    arena.offset = 0;
 
     return arena;
 }
 
-void arena_destroy(mem_arena* arena) {
-    if (arena) {
-        plat_mem_release(arena, arena->reserve_size);
-    }
-}
-
-void* arena_push(mem_arena* arena, u64 size, b32 non_zero) {
-    u64 pos_aligned = ALIGN_UP_POW(arena->pos, ARENA_ALIGN);
-    u64 new_pos = pos_aligned + size;
-    
-    if (new_pos > arena->reserve_size) { return NULL; }
-
-    if (new_pos > arena->commit_pos) {
-        u64 new_commit_pos = new_pos;
-        new_commit_pos += arena->commit_size - 1;
-        new_commit_pos -= new_commit_pos % arena->commit_size;
-        new_commit_pos = MIN(new_commit_pos, arena->reserve_size);
-
-        u8* mem = (u8*)arena + arena->commit_pos;
-        u64 commit_size = new_commit_pos - arena->commit_pos;
-
-        if (!plat_mem_commit(mem, commit_size)) {
-            return NULL;
-        }
-        
-        arena->commit_pos = new_commit_pos;
+void* alloc_arena(mem_arena* arena, u64 size) {
+    if (!arena->memory_block) {
+        std::cerr << "WARN: Arena not initialized\n";
+        return nullptr;
     }
 
-    arena->pos = new_pos;
-    u8* out = (u8*)arena + pos_aligned;
-
-    if (!non_zero) {
-        memset(out, 0, size);
+    if (arena->offset + size > arena->capacity) {
+        std::cerr << "WARN: Arena out of memory\n";
+        return nullptr;
     }
 
-    return out;
+    u8* result = arena->memory_block + arena->offset;
+    arena->offset += size;
+
+    return result;
 }
 
-void arena_pop(mem_arena* arena, u64 size) {
-    size = MIN(size, arena->pos - ARENA_BASE_POS);
-    arena->pos -= size;
+void reset_arena(mem_arena* arena) {
+    if (arena->capacity == 0) {
+        std::cerr << "WARN: Attempt to reset zeroed Arena.\n";
+    }
+
+    arena->offset = 0;
 }
 
-void arena_pop_to(mem_arena* arena, u64 pos) {
-    u64 size = pos < arena->pos ? arena->pos - pos : 0;
-    arena_pop(arena, size);
-}
+void void_arena(mem_arena* arena) {
+    if (!(arena->memory_block)) {
+        throw std::runtime_error("Attempt to free uninitialized Arena");
+    } else {
+        free(arena->memory_block);
+    }
 
-void arena_clear(mem_arena* arena) {
-    arena_pop_to(arena, ARENA_BASE_POS);
+    arena->memory_block = nullptr;
+    arena->capacity = 0;
+    arena->offset = 0;
 }
-
-#ifdef _WIN32
-#include <Windows.h>
-
-u32 plat_get_pagesize(void) {
-    SYSTEM_INFO sysinfo;
-    GetSystemInfo(&sysinfo);
-    return sysinfo.dwPageSize;
-}
-
-void* plat_mem_reserve(u64 size) {
-    return VirtualAlloc(NULL, size, MEM_RESERVE, PAGE_READWRITE);
-}
-
-b32 plat_mem_commit(void* ptr, u64 size) {
-    void* ret = VirtualAlloc(ptr, size, MEM_COMMIT, PAGE_READWRITE);
-    return ret != NULL;
-}
-
-b32 plat_mem_decommit(void* ptr, u64 size) {
-    return VirtualFree(ptr, size, MEM_DECOMMIT);
-}
-
-b32 plat_mem_release(void* ptr, u64 size) {
-    return VirtualFree(ptr, size, MEM_RELEASE);
-}
-#endif
